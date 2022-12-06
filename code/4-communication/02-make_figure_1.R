5b736ac01538c102fa1599e2805c5cb12efbca1f
David Gonzalez <djxgonzalez@gmail.com>
2022-12-05 18:31:13 -0800

    editing figure 1 script

--- code/4-communication/02-make_figure_1.R
+++ code/4-communication/02-make_figure_1.R
@@ -10,8 +10,8 @@ library("ggspatial")
 # data input, prep layers for mapping ......................................
 us_states_west <- st_read("data/raw/us_census/tl_2018_us_state.shp") %>% 
   filter(STUSPS %in% c("WA", "OR", "CA", "ID", "NV", "AZ", "MT", "WY", "UT",
-                      "CO", "NM", "ND", "SD", "NE", "KS", "OK", "TX", "MO",
-                      "AR", "LA")) %>%
+                       "CO", "NM", "ND", "SD", "NE", "KS", "OK", "TX", "MO",
+                       "AR", "LA")) %>%
   st_geometry() %>%
   st_transform(crs_nad83)
 us_states_east <- st_read("data/raw/us_census/tl_2018_us_state.shp") %>%
@@ -32,90 +32,118 @@ rus_can <- st_read("data/raw/esri/Countries_WGS84.shp") %>%
   st_geometry() %>%
   st_transform(crs_nad83)
 
-##### note: we need to re-generate these datasets to add the new states (MN>LA)
 # fires data
-wildfires_1984 <- readRDS("data/interim/wildfires_union_contiguous_1984.rds") %>%
+wildfires_1984 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1984.rds") %>%
   st_geometry()
-wildfires_1985 <- readRDS("data/interim/wildfires_union_contiguous_1985.rds") %>%
+wildfires_1985 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1985.rds") %>%
   st_geometry()
-wildfires_1986 <- readRDS("data/interim/wildfires_union_contiguous_1986.rds") %>%
+wildfires_1986 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1986.rds") %>%
   st_geometry()
-wildfires_1987 <- readRDS("data/interim/wildfires_union_contiguous_1987.rds") %>%
+wildfires_1987 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1987.rds") %>%
   st_geometry()
-wildfires_1988 <- readRDS("data/interim/wildfires_union_contiguous_1988.rds") %>%
+wildfires_1988 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1988.rds") %>%
   st_geometry()
-wildfires_1989 <- readRDS("data/interim/wildfires_union_contiguous_1989.rds") %>%
+wildfires_1989 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1989.rds") %>%
   st_geometry()
-wildfires_1990 <- readRDS("data/interim/wildfires_union_contiguous_1990.rds") %>%
+wildfires_1990 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1990.rds") %>%
   st_geometry()
-wildfires_1991 <- readRDS("data/interim/wildfires_union_contiguous_1991.rds") %>%
+wildfires_1991 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1991.rds") %>%
   st_geometry()
-wildfires_1992 <- readRDS("data/interim/wildfires_union_contiguous_1992.rds") %>%
+wildfires_1992 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1992.rds") %>%
   st_geometry()
-wildfires_1993 <- readRDS("data/interim/wildfires_union_contiguous_1993.rds") %>%
+wildfires_1993 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1993.rds") %>%
   st_geometry()
-wildfires_1994 <- readRDS("data/interim/wildfires_union_contiguous_1994.rds") %>%
+wildfires_1994 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1994.rds") %>%
   st_geometry()
-wildfires_1995 <- readRDS("data/interim/wildfires_union_contiguous_1995.rds") %>%
+wildfires_1995 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1995.rds") %>%
   st_geometry()
-wildfires_1996 <- readRDS("data/interim/wildfires_union_contiguous_1996.rds") %>%
+wildfires_1996 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1996.rds") %>%
   st_geometry()
-wildfires_1997 <- readRDS("data/interim/wildfires_union_contiguous_1997.rds") %>%
+wildfires_1997 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1997.rds") %>%
   st_geometry()
-wildfires_1998 <- readRDS("data/interim/wildfires_union_contiguous_1998.rds") %>%
+wildfires_1998 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1998.rds") %>%
   st_geometry()
-wildfires_1999 <- readRDS("data/interim/wildfires_union_contiguous_1999.rds") %>%
+wildfires_1999 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_1999.rds") %>%
   st_geometry()
-wildfires_2000 <- readRDS("data/interim/wildfires_union_contiguous_2000.rds") %>%
+wildfires_2000 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2000.rds") %>%
   st_geometry()
-wildfires_2001 <- readRDS("data/interim/wildfires_union_contiguous_2001.rds") %>%
+wildfires_2001 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2001.rds") %>%
   st_geometry()
-wildfires_2002 <- readRDS("data/interim/wildfires_union_contiguous_2002.rds") %>%
+wildfires_2002 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2002.rds") %>%
   st_geometry()
-wildfires_2003 <- readRDS("data/interim/wildfires_union_contiguous_2003.rds") %>%
+wildfires_2003 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2003.rds") %>%
   st_geometry()
-wildfires_2004 <- readRDS("data/interim/wildfires_union_contiguous_2004.rds") %>%
+wildfires_2004 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2004.rds") %>%
   st_geometry()
-wildfires_2005 <- readRDS("data/interim/wildfires_union_contiguous_2005.rds") %>%
+wildfires_2005 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2005.rds") %>%
   st_geometry()
-##### error making 2006 data; need to fix
-# wildfires_2006 <- readRDS("data/interim/wildfires_union_contiguous_2006.rds") %>%
-#   st_geometry()
-wildfires_2007 <- readRDS("data/interim/wildfires_union_contiguous_2007.rds") %>%
+wildfires_2006 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2006.rds") %>%
   st_geometry()
-wildfires_2008 <- readRDS("data/interim/wildfires_union_contiguous_2008.rds") %>%
+wildfires_2007 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2007.rds") %>%
   st_geometry()
-wildfires_2009 <- readRDS("data/interim/wildfires_union_contiguous_2009.rds") %>%
+wildfires_2008 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2008.rds") %>%
   st_geometry()
-wildfires_2010 <- readRDS("data/interim/wildfires_union_contiguous_2010.rds") %>%
+wildfires_2009 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2009.rds") %>%
   st_geometry()
-wildfires_2011 <- readRDS("data/interim/wildfires_union_contiguous_2011.rds") %>%
+wildfires_2010 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2010.rds") %>%
   st_geometry()
-wildfires_2012 <- readRDS("data/interim/wildfires_union_contiguous_2012.rds") %>%
+wildfires_2011 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2011.rds") %>%
   st_geometry()
-wildfires_2013 <- readRDS("data/interim/wildfires_union_contiguous_2013.rds") %>%
+wildfires_2012 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2012.rds") %>%
   st_geometry()
-wildfires_2014 <- readRDS("data/interim/wildfires_union_contiguous_2014.rds") %>%
+wildfires_2013 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2013.rds") %>%
   st_geometry()
-wildfires_2015 <- readRDS("data/interim/wildfires_union_contiguous_2015.rds") %>%
+wildfires_2014 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2014.rds") %>%
   st_geometry()
-wildfires_2016 <- readRDS("data/interim/wildfires_union_contiguous_2016.rds") %>%
+wildfires_2015 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2015.rds") %>%
   st_geometry()
-wildfires_2017 <- readRDS("data/interim/wildfires_union_contiguous_2017.rds") %>%
+wildfires_2016 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2016.rds") %>%
   st_geometry()
-wildfires_2018 <- readRDS("data/interim/wildfires_union_contiguous_2018.rds") %>%
+wildfires_2017 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2017.rds") %>%
   st_geometry()
-wildfires_2019 <- readRDS("data/interim/wildfires_union_contiguous_2019.rds") %>%
+wildfires_2018 <-
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2018.rds") %>%
   st_geometry()
-wildfires_2020 <- readRDS("data/interim/wildfires_union_contiguous_2020.rds") %>%
+wildfires_2019 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2019.rds") %>%
   st_geometry()
-
-
-##### may need to edit this; computationally intensive!
-
-
-####
-
+wildfires_2020 <- 
+  readRDS("data/interim/buffers_wildfire_years/wildfires_union_contiguous_2020.rds") %>%
+  st_geometry()
 
 # lakes <- st_read("data/raw/noaa/gshhg-shp-2.3.7/GSHHS_shp/l/GSHHS_l_L2.shp") %>%
 #   st_make_valid() %>%
@@ -131,8 +159,8 @@ wildfires_2020 <- readRDS("data/interim/wildfires_union_contiguous_2020.rds") %>
 
 # makes figure
 figure_1a <- ggplot() +
-  geom_sf(data = mex_can, fill = "#DCDCDC", color = NA, alpha = 0.7) +
-  geom_sf(data = us_states_east, fill = "#DCDCDC", color = "#ffffff", lwd = 0.3) +
+  # geom_sf(data = mex_can, fill = "#DCDCDC", color = NA, alpha = 0.7) +
+  # geom_sf(data = us_states_east, fill = "#DCDCDC", color = "#ffffff", lwd = 0.3) +
   geom_sf(data = us_states_west, fill = "#E6E9E0", color = "#ffffff", lwd = 0.3) +
   geom_sf(data = wildfires_1984, fill = "purple", color = NA, alpha = 0.3) +
   geom_sf(data = wildfires_1985, fill = "purple", color = NA, alpha = 0.3) +
@@ -156,24 +184,24 @@ figure_1a <- ggplot() +
   geom_sf(data = wildfires_2003, fill = "orange", color = NA, alpha = 0.6) +
   geom_sf(data = wildfires_2004, fill = "orange", color = NA, alpha = 0.6) +
   geom_sf(data = wildfires_2005, fill = "orange", color = NA, alpha = 0.6) +
-  # geom_sf(data = wildfires_2006, fill = "orange", color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2006, fill = "orange", color = NA, alpha = 0.6) +
   geom_sf(data = wildfires_2007, fill = "orange", color = NA, alpha = 0.6) +
   geom_sf(data = wildfires_2008, fill = "orange", color = NA, alpha = 0.6) +
   geom_sf(data = wildfires_2009, fill = "orange", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2010, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2011, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2012, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2013, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2014, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2015, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2016, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2017, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2018, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2019, fill = "red", color = NA, alpha = 0.6) +
-  geom_sf(data = wildfires_2020, fill = "red", color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2010, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2011, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2012, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2013, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2014, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2015, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2016, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2017, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2018, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2019, fill = "red",    color = NA, alpha = 0.6) +
+  geom_sf(data = wildfires_2020, fill = "red",    color = NA, alpha = 0.6) +
   #geom_sf(data = lakes, fill = "#e9f5f8", color = NA) +  ##### may not need this
   ##### add wildfires layer
-  xlim(-125, -89) + ylim(26, 49) +
+  #xlim(-125, -89) + ylim(26, 49) +
   labs(x = "", y = "") +
   theme_bw() +
   theme(panel.background = element_rect(fill  = "#e9f5f8"),  # d0ecfd
