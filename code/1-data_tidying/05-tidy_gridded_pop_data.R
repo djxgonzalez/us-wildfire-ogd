526afbff31a60812d111a992fef2d1a2bcda1519
David Gonzalez <djxgonzalez@gmail.com>
2022-12-20 15:57:05 -0800

    Added script to tidy gridded pop data

---
+++ code/1-data_tidying/05-tidy_gridded_pop_data.R
@@ -0,0 +1,29 @@
+##============================================================================##
+## 1.05 - imports and prepares SocScape gridded population data for assessment
+## of the number of people who reside near wells that burned in wildfires
+
+## setup ---------------------------------------------------------------------
+
+# attaches packages we need for this script
+source("code/0-setup/01-setup.R")
+library("stars")
+
+# data input
+pop_gridded_1990 <- read_stars("data/raw/socscape/us_pop2010myc.tif")
+
+us_states_west <- st_read("data/raw/esri/USA_States_Generalized.shp") %>% 
+  filter(STATE_ABBR %in% c("WA", "OR", "CA", "ID", "NV", "AZ", "MT", "WY", "UT",
+                           "CO", "NM", "ND", "SD", "NE", "KS", "OK", "TX", "MO",
+                           "AR", "LA")) %>%
+  st_transform(crs_albers)
+
+
+## 1990 ----------------------------------------------------------------------
+
+# data prep ................................................................
+
+# gets necessary variables as numeric vectors[ri]
+
+
+
+##============================================================================##
